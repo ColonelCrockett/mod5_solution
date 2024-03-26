9dca095b331a6b8d430c51d70a12a02ffbddcf20
ColonelCrockett <163603913+ColonelCrockett@users.noreply.github.com>
2024-03-26 17:49:59 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -147,11 +147,9 @@ $ajaxUtils.sendGetRequest(
       // var homeHtmlToInsertIntoMainPage = ....
 
       // STEP 3: Substitute {{randomCategoryShortName}} in the home HTML snippet with the chosen category from Step 2
-    var homeHtmlToInsertIntoMainPage = homeHtml.replace('REPLACE_SHORT_NAME', "'" + chosenCategoryShortName + "'");
+    var homeHtmlToInsertIntoMainPage = homeHtml.replace(/REPLACE_SHORT_NAME/g, "'" + chosenCategoryShortName + "'");
 
-
-
-      // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
+  // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
